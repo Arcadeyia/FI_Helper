d97cd5a4479298e8728d3d8f9de014b4a27b8bcb
nyxb <contact@nyxb.email>
2023-11-23 00:16:28 +0100

    ✨ feat(Discord/commands/user/getreport.js): Überarbeitet Befehlsstruktur und verbessert die Benutzerinteraktion.
    Die Änderungen umfassen die Aktualisierung der Befehlsdefinition, um eine neue Bibliothek zu verwenden, die Verbesserung der Benutzerinteraktion durch direktes Senden von Nachrichten und die Bereitstellung klarerer Rückmeldungen.

--- Discord/commands/user/getreport.js
+++ Discord/commands/user/getreport.js
@@ -1,26 +1,43 @@
-const { SlashCommandBuilder } = require('discord.js')
+const { CommandType } = require('@nyxb/commands')
 const report = require('../../report.js')
 const config = require('../../config.js')
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('reports')
-    .setDescription('Receive Reports.')
-    .addSubcommand(subcommand =>
-      subcommand
-        .setName('all')
-        .setDescription('Get all available class reports'),
-    )
-    .addSubcommand(subcommand =>
-      subcommand
-        .setName('week')
-        .setDescription('Get class report from a specific week')
-        .addIntegerOption(option =>
-          option.setName('kw')
-            .setDescription('The week you want to receive')
-            .setRequired(true))),
-  async execute(interaction) {
-    const subcmd = interaction.options.getSubcommand()
+  category: 'Information',
+  description: 'Receive Reports.',
+
+  slash: CommandType.BOTH,
+  testOnly: true,
+  guildOnly: true,
+
+  options: [
+    {
+      name: 'type',
+      description: 'Type of report to fetch',
+      type: 'STRING',
+      required: true,
+      choices: [
+        {
+          name: 'All',
+          value: 'all',
+        },
+        {
+          name: 'Week',
+          value: 'week',
+        },
+      ],
+    },
+    {
+      name: 'week',
+      description: 'The week number for the report',
+      type: 'INTEGER',
+      required: false,
+    },
+  ],
+
+  callback: async ({ interaction }) => {
+    const type = interaction.options.getString('type')
+    const week = interaction.options.getInteger('week')
     let class_name
 
     Object.entries(config.classes).forEach(([className, classConfig]) => {
@@ -28,29 +45,28 @@ module.exports = {
         class_name = className
     })
 
-    if (!class_name) {
-      await interaction.reply({ content: `You do not have a role associated with any class.`, ephemeral: true })
-      return
-    }
+    if (!class_name)
+      return `You do not have a role associated with any class.`
 
-    if (subcmd === 'all') {
-      let counter = 0
+    if (type === 'all') {
       const report_packets = report.getAllReports(class_name)
-      for (const packet of report_packets) {
-        counter++
-        interaction.user.send({ content: `All available Reports for ${class_name}! ${counter}/${report_packets.length}`, files: packet })
+      report_packets.forEach((packet, index) => {
+        interaction.user.send({ content: `All available Reports for ${class_name}! ${index + 1}/${report_packets.length}`, files: packet })
+      })
+      return `Sending a DM with all Reports for ${class_name}!`
+    }
+    else if (type === 'week') {
+      if (!week)
+        return `Please provide a week number.`
+
+      const report_files = report.getReportFromWeek(class_name, week)
+      if (report_files) {
+        interaction.user.send({ content: `Reports for ${class_name} from week ${week}`, files: report_files })
+        return `Sending a DM with Reports for ${class_name} for week ${week}.`
+      }
+      else {
+        return `No available Reports for ${class_name} for week ${week}`
       }
     }
-    else {
-      const week = interaction.options.getInteger('kw')
-      const report_files = report.getReportFromWeek(class_name, week)
-      if (report_files)
-        interaction.user.send({ content: `Reports for ${class_name} from week ${week}`, files: report_files })
-
-      else
-        interaction.user.send(`No available Reports for ${class_name} for week ${week}`)
-    }
-
-    await interaction.reply({ content: `Send a DM with the Files for ${class_name}!`, ephemeral: true })
   },
 }
