ae5d74ad4f547176a1fffd6fcdcccff31a4d33c7
nyxb <contact@nyxb.email>
2023-11-22 22:13:41 +0100

    ✨ feat(Discord/utils): Fügt eine neue Konfigurationsdatei für Klassenrollen und Kanal-IDs hinzu.

---
+++ Discord/utils/config.js
@@ -0,0 +1,61 @@
+const config = {
+  baseUrl: 'https://service.viona24.com/stpusnl/daten/US_IT_2023_Sommer_Aug_',
+
+  classes: {
+    FIAE_A: {
+      roleId: '1126107038657810513',
+      reportChannelId: '1136207446273179760',
+      scheduleChannelId: '1136207481475960914',
+    },
+    FIAE_B: {
+      roleId: '1136209520381657110',
+      reportChannelId: '1125803107675283487',
+      scheduleChannelId: '1125797133325451304',
+    },
+    FIAE_C: {
+      roleId: '1143065481222303784',
+      reportChannelId: '1150736984805998592',
+      scheduleChannelId: '1150737060395761686',
+    },
+    FIAE_D: {
+      roleId: '1151937349996007455',
+      reportChannelId: '1151938291365580874',
+      scheduleChannelId: '1151938348236148756',
+    },
+    FISI_A_1: {
+      roleId: '1126106908210770010',
+      reportChannelId: '1136994959111770182',
+      scheduleChannelId: '1136995021627867207',
+    },
+    FISI_A_2: {
+      roleId: '1141275856115875891',
+      reportChannelId: '1141629991549927534',
+      scheduleChannelId: '1141630027402854442',
+    },
+    FISI_B: {
+      roleId: '1136209669392699483',
+      reportChannelId: '1125796045838876743',
+      scheduleChannelId: '1125802200044687480',
+    },
+    FISI_C: {
+      roleId: '1173944751775547452',
+      reportChannelId: '1173987349634490448',
+      scheduleChannelId: '1173987507873001502',
+    },
+    FISI_D: {
+      roleId: '1173944880335171595',
+      reportChannelId: '1173981517207322657',
+      scheduleChannelId: '1173981648098971668',
+    },
+    FISI_E: {
+      roleId: '1173944617192919040',
+      reportChannelId: '1173985878155202590',
+      scheduleChannelId: '1173985913701937173',
+    },
+    // Weitere Klassen und deren Zuordnungen...
+  },
+
+  // Weitere Konfigurationselemente nach Bedarf...
+}
+
+module.exports = config
