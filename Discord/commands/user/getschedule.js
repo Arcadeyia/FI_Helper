3e3fc401a588a12d66eb4aa79f96457702bf8e92
nyxb <contact@nyxb.email>
2023-11-23 00:16:42 +0100

    ✨ feat(Discord/commands/user/getschedule.js): Aktualisiert Befehlsstruktur und verbessert die Benutzerinteraktion.

--- Discord/commands/user/getschedule.js
+++ Discord/commands/user/getschedule.js
@@ -1,57 +1,72 @@
-const { SlashCommandBuilder } = require('discord.js')
+const { CommandType } = require('@nyxb/commands')
 const schedule = require('../../schedulers/schedule.js')
-const config = require('../../config.js') // Import der neuen Konfiguration
+const config = require('../../config.js')
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('schedule')
-    .setDescription('Receive Schedules.')
-    .addSubcommand(subcommand =>
-      subcommand
-        .setName('all')
-        .setDescription('Get all available class schedules'),
-    )
-    .addSubcommand(subcommand =>
-      subcommand
-        .setName('week')
-        .setDescription('Get class schedules from a specific week')
-        .addIntegerOption(option =>
-          option.setName('kw')
-            .setDescription('The week you want to receive')
-            .setRequired(true))),
-  async execute(interaction) {
-    const subcmd = interaction.options.getSubcommand()
+  category: 'Schedule', // Kategorie des Befehls
+  description: 'Receive Schedules.', // Beschreibung des Befehls
+
+  slash: CommandType.BOTH,
+  testOnly: true,
+  guildOnly: true,
+
+  options: [
+    {
+      name: 'type',
+      description: 'Type of schedule to fetch',
+      type: 'STRING',
+      required: true,
+      choices: [
+        {
+          name: 'All',
+          value: 'all',
+        },
+        {
+          name: 'Week',
+          value: 'week',
+        },
+      ],
+    },
+    {
+      name: 'week',
+      description: 'The week number for the schedule',
+      type: 'INTEGER',
+      required: false,
+    },
+  ],
+
+  callback: async ({ interaction }) => {
+    const type = interaction.options.getString('type')
+    const week = interaction.options.getInteger('week')
     let class_name
 
-    // Finden der entsprechenden Klasse basierend auf der Rolle des Benutzers
     Object.entries(config.classes).forEach(([className, classConfig]) => {
       if (interaction.member.roles.cache.has(classConfig.roleId))
         class_name = className
     })
 
-    if (!class_name) {
-      await interaction.reply({ content: `You do not have a role associated with any class.`, ephemeral: true })
-      return
-    }
+    if (!class_name)
+      return `You do not have a role associated with any class.`
 
-    if (subcmd === 'all') {
-      let counter = 0
+    if (type === 'all') {
       const schedule_packets = schedule.getAllSchedules(class_name)
-      for (const packet of schedule_packets) {
-        counter++
-        interaction.user.send({ content: `All available Schedules for ${class_name}! ${counter}/${schedule_packets.length}`, files: packet })
+      schedule_packets.forEach((packet, index) => {
+        interaction.user.send({ content: `All available Schedules for ${class_name}! ${index + 1}/${schedule_packets.length}`, files: packet })
+      })
+      return `Sending a DM with all Schedules for ${class_name}!`
+    }
+    else if (type === 'week') {
+      if (!week)
+        return `Please provide a week number.`
+
+      const schedule_files = schedule.getScheduleFromWeek(class_name, week)
+      if (schedule_files) {
+        interaction.user.send({ content: `Schedules for ${class_name} from week ${week}`, files: schedule_files })
+        return `Sending a DM with Schedules for ${class_name} for week ${week}.`
+      }
+      else {
+        return `No available Schedules for ${class_name} for week ${week}`
       }
     }
-    else {
-      const week = interaction.options.getInteger('kw')
-      const schedule_files = schedule.getScheduleFromWeek(class_name, week)
-      if (schedule_files)
-        interaction.user.send({ content: `Schedules for ${class_name} from week ${week}`, files: schedule_files })
-
-      else
-        interaction.user.send(`No available Schedule for ${class_name} for week ${week}`)
-    }
-
-    await interaction.reply({ content: `Send a DM with the Files for ${class_name}!`, ephemeral: true })
   },
 }
