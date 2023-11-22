37dc23f54ae16c4d5a651ad7552d5eaa923038a7
nyxb <contact@nyxb.email>
2023-11-22 18:00:09 +0100

    style:🎨 refactor(Discord/commands/user/getreport.js): Vereinheitlicht die Verwendung von Anführungszeichen und entfernt überflüssige Semikolons.

--- Discord/commands/user/getreport.js
+++ Discord/commands/user/getreport.js
@@ -1,51 +1,51 @@
-const { SlashCommandBuilder } = require('discord.js');
-const report = require("../../report.js")
-const settings = require("../../settings.json")
+const { SlashCommandBuilder } = require('discord.js')
+const report = require('../../report.js')
+const settings = require('../../settings.json')
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('reports')
-        .setDescription('Receive Reports.')
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('all')
-                .setDescription('Get all available class reports')
-        )
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('week')
-                .setDescription('Get class report from a specific week')
-                .addIntegerOption(option =>
-                    option.setName('kw')
-                        .setDescription('The week you want to receive')
-                        .setRequired(true))),
-    async execute(interaction) {
-        const subcmd = interaction.options.getSubcommand()
+  data: new SlashCommandBuilder()
+    .setName('reports')
+    .setDescription('Receive Reports.')
+    .addSubcommand(subcommand =>
+      subcommand
+        .setName('all')
+        .setDescription('Get all available class reports'),
+    )
+    .addSubcommand(subcommand =>
+      subcommand
+        .setName('week')
+        .setDescription('Get class report from a specific week')
+        .addIntegerOption(option =>
+          option.setName('kw')
+            .setDescription('The week you want to receive')
+            .setRequired(true))),
+  async execute(interaction) {
+    const subcmd = interaction.options.getSubcommand()
 
-        for (const role_id in settings.roles) {
-            if (interaction.member.roles.cache.has(role_id)) {
-                class_name = settings.roles[role_id]
-                break
-            }
-        }
+    for (const role_id in settings.roles) {
+      if (interaction.member.roles.cache.has(role_id)) {
+        class_name = settings.roles[role_id]
+        break
+      }
+    }
 
-        if (subcmd == "all") {
-            var counter = 0
-            report_packets = report.getAllReports(class_name)
-            for (const packet of report_packets) {
-                counter++
-                interaction.user.send({ content: `All available Reports for ${class_name}! ${counter}/${report_packets.length}`, files: packet })
-            }
-        } else {
-            const week = interaction.options.getInteger("kw")
-            report_files = report.getReportFromWeek(class_name, week)
-            if (report_files) {
-                interaction.user.send({ content: `Reports for ${class_name} from week ${week}`, files: report_files })
-            } else {
-                interaction.user.send(`No available Reports for ${class_name} for week ${week}`)
+    if (subcmd == 'all') {
+      let counter = 0
+      report_packets = report.getAllReports(class_name)
+      for (const packet of report_packets) {
+        counter++
+        interaction.user.send({ content: `All available Reports for ${class_name}! ${counter}/${report_packets.length}`, files: packet })
+      }
+    }
+    else {
+      const week = interaction.options.getInteger('kw')
+      report_files = report.getReportFromWeek(class_name, week)
+      if (report_files)
+        interaction.user.send({ content: `Reports for ${class_name} from week ${week}`, files: report_files })
 
-            }
-        }
-        await interaction.reply({ content: `Send a DM with the Files for ${class_name}!`, ephemeral: true });
-    },
-};
+      else
+        interaction.user.send(`No available Reports for ${class_name} for week ${week}`)
+    }
+    await interaction.reply({ content: `Send a DM with the Files for ${class_name}!`, ephemeral: true })
+  },
+}
