fe0c30da51d9356bcb26056642c4d99e578997f8
nyxb <contact@nyxb.email>
2023-11-22 22:39:34 +0100

    refactor:♻️(getschedule.js): Ersetzt settings.json durch config.js und verbessert die Rollenüberprüfung.

--- Discord/commands/user/getschedule.js
+++ Discord/commands/user/getschedule.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js')
 const schedule = require('../../schedulers/schedule.js')
-const settings = require('../../settings.json')
+const config = require('../../config.js') // Import der neuen Konfiguration
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -21,17 +21,22 @@ module.exports = {
             .setRequired(true))),
   async execute(interaction) {
     const subcmd = interaction.options.getSubcommand()
+    let class_name
 
-    for (const role_id in settings.roles) {
-      if (interaction.member.roles.cache.has(role_id)) {
-        class_name = settings.roles[role_id]
-        break
-      }
+    // Finden der entsprechenden Klasse basierend auf der Rolle des Benutzers
+    Object.entries(config.classes).forEach(([className, classConfig]) => {
+      if (interaction.member.roles.cache.has(classConfig.roleId))
+        class_name = className
+    })
+
+    if (!class_name) {
+      await interaction.reply({ content: `You do not have a role associated with any class.`, ephemeral: true })
+      return
     }
 
-    if (subcmd == 'all') {
+    if (subcmd === 'all') {
       let counter = 0
-      schedule_packets = schedule.getAllSchedules(class_name)
+      const schedule_packets = schedule.getAllSchedules(class_name)
       for (const packet of schedule_packets) {
         counter++
         interaction.user.send({ content: `All available Schedules for ${class_name}! ${counter}/${schedule_packets.length}`, files: packet })
@@ -39,13 +44,14 @@ module.exports = {
     }
     else {
       const week = interaction.options.getInteger('kw')
-      schedule_files = schedule.getScheduleFromWeek(class_name, week)
+      const schedule_files = schedule.getScheduleFromWeek(class_name, week)
       if (schedule_files)
         interaction.user.send({ content: `Schedules for ${class_name} from week ${week}`, files: schedule_files })
 
       else
         interaction.user.send(`No available Schedule for ${class_name} for week ${week}`)
     }
+
     await interaction.reply({ content: `Send a DM with the Files for ${class_name}!`, ephemeral: true })
   },
 }
