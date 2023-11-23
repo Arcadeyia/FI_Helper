b03ea9a4b89c82bccb164dd237d8d0dfb5f57216
nyxb <contact@nyxb.email>
2023-11-23 22:39:25 +0100

    ✨ feat(Discord/bot.js): Struktur der Projektdateien verbessert und Pfade aktualisiert
    ♻️ refactor(Discord/bot.js): Verwendung von async/await für bessere Fehlerbehandlung beim Erstellen von Ordnern
    
    🩹 fix(Discord/bot.js): Standardbefehle von NYXB deaktiviert, um benutzerdefinierte Funktionalität zu ermöglichen
    
    🎨 style(Discord/bot.js): Kommentare aktualisiert, um Klarheit zu verbessern

--- Discord/bot.js
+++ Discord/bot.js
@@ -3,10 +3,12 @@ const path = require('node:path')
 const dotenv = require('dotenv')
 const { Client, Events, IntentsBitField, Partials } = require('discord.js')
 const NYXB = require('@nyxb/commands')
+
+const { DefaultCommands } = NYXB
 const cj = require('consolji')
-const config = require('./config.js') // Importieren der neuen Konfigurationsdatei
-const initializeCronJobs = require('./cronJobHandler.js')
-const createFolders = require('./folderHandler.js')
+const config = require('./utils/config.js')
+const initializeCronJobs = require('./handler/cronJobHandler.js')
+const createFolders = require('./handler/folderHandler.js')
 
 dotenv.config({ path: 'cfg.env' })
 
@@ -15,25 +17,36 @@ const client = new Client({
   partials: [Partials.Message, Partials.Channel, Partials.Reaction, Partials.User, Partials.GuildMember, Partials.GuildScheduledEvent],
 })
 
-client.once(Events.ClientReady, () => {
+client.once(Events.ClientReady, async () => {
   cj.log('🤖 Beep Boop i\'m Ready!')
 
-  Object.entries(config.classes).forEach(([_, classConfig]) => {
-    const class_name = classConfig.roleId
-    createFolders(class_name)
-  })
+  try {
+    for (const klassenName of Object.keys(config.klassen))
+      await createFolders(klassenName)
+  }
+  catch (error) {
+    console.error('Error creating folders:', error)
+  }
 
   initializeCronJobs(client)
 
-  new NYXB(client, {
+  new NYXB({
+    client,
     commandsDir: path.join(__dirname, 'commands'),
     testServers: [config.testServerId],
+    disabledDefaultCommands: [
+      DefaultCommands.ChannelCommand,
+      DefaultCommands.CustomCommand,
+      DefaultCommands.Prefix,
+      DefaultCommands.RequiredPermissions,
+      DefaultCommands.RequiredRoles,
+      DefaultCommands.ToggleCommand,
+    ],
   })
 
-  cj.log('Scheduled Cron Job!')
+  cj.log('Stundenplan Cron Job!')
 })
 
-// Verwenden des entsprechenden Tokens basierend auf der Umgebung
 client.login(
   process.env.PROD === 'true'
     ? process.env.PROD_DISCORD_TOKEN
