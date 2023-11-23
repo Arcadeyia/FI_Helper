870e2f8f03b0430affbbf7ce838cbf0bb5d76a5d
nyxb <contact@nyxb.email>
2023-11-23 22:39:46 +0100

    refactor:♻️(getreport.js): Überarbeitung der Befehlsstruktur und Verbesserung der Importpfade
    feat:✨(getreport.js): Einführung von Subcommands für bessere Befehlsstrukturierung

--- Discord/commands/user/getreport.js
+++ Discord/commands/user/getreport.js
@@ -1,72 +1,66 @@
-const { CommandType } = require('@nyxb/commands')
-const report = require('../../report.js')
-const config = require('../../config.js')
+const { CommandType } = require('@nyxb/commands');
+const { ApplicationCommandOptionType } = require('discord.js');
+const report = require('../../utils/report.js');
+const config = require('../../utils/config.js');
+
 
 module.exports = {
-  category: 'Information',
-  description: 'Receive Reports.',
+   category: 'Information',
+   description: 'Receive Reports.',
+   type: CommandType.BOTH,
 
-  slash: CommandType.BOTH,
-  testOnly: true,
-  guildOnly: true,
+   options: [
+      {
+         name: 'all',
+         description: 'Get all available class reports',
+         type: ApplicationCommandOptionType.Subcommand, // Stellen Sie sicher, dass dies korrekt ist
+      },
+      {
+         name: 'week',
+         description: 'Get class report from a specific week',
+         type: ApplicationCommandOptionType.Subcommand,
+         options: [
+            {
+               name: 'kw',
+               description: 'The week you want to receive',
+               type: ApplicationCommandOptionType.Integer, // Stellen Sie sicher, dass dies korrekt ist
+               required: true,
+            },
+         ],
+      },
+   ],
 
-  options: [
-    {
-      name: 'type',
-      description: 'Type of report to fetch',
-      type: 'STRING',
-      required: true,
-      choices: [
-        {
-          name: 'All',
-          value: 'all',
-        },
-        {
-          name: 'Week',
-          value: 'week',
-        },
-      ],
-    },
-    {
-      name: 'week',
-      description: 'The week number for the report',
-      type: 'INTEGER',
-      required: false,
-    },
-  ],
+   callback: ({ interaction, args }) => {
+      let klassenName = '';
 
-  callback: async ({ interaction }) => {
-    const type = interaction.options.getString('type')
-    const week = interaction.options.getInteger('week')
-    let class_name
+      // Logik zur Bestimmung des klassenName basierend auf Rollen
+      Object.entries(config.classes).forEach(([_, klassenConfig]) => {
+         if (interaction.member.roles.cache.has(klassenConfig.rollenId))
+            klassenName = klassenConfig.klassenName;
+      });
 
-    Object.entries(config.classes).forEach(([className, classConfig]) => {
-      if (interaction.member.roles.cache.has(classConfig.roleId))
-        class_name = className
-    })
+      if (!klassenName)
+         return interaction.reply({ content: `You do not have a role associated with any class.`, ephemeral: true });
 
-    if (!class_name)
-      return `You do not have a role associated with any class.`
+      const type = args.find(arg => arg.name === 'type').value;
+      const week = args.find(arg => arg.name === 'week')?.value;
 
-    if (type === 'all') {
-      const report_packets = report.getAllReports(class_name)
-      report_packets.forEach((packet, index) => {
-        interaction.user.send({ content: `All available Reports for ${class_name}! ${index + 1}/${report_packets.length}`, files: packet })
-      })
-      return `Sending a DM with all Reports for ${class_name}!`
-    }
-    else if (type === 'week') {
-      if (!week)
-        return `Please provide a week number.`
-
-      const report_files = report.getReportFromWeek(class_name, week)
-      if (report_files) {
-        interaction.user.send({ content: `Reports for ${class_name} from week ${week}`, files: report_files })
-        return `Sending a DM with Reports for ${class_name} for week ${week}.`
+      if (type === 'all') {
+         const report_packets = report.getAllReports(klassenName);
+         report_packets.forEach((packet, index) => {
+            interaction.user.send({ content: `All available Reports for ${klassenName}! ${index + 1}/${report_packets.length}`, files: packet });
+         });
+         return `Sending a DM with all Reports for ${klassenName}!`;
       }
-      else {
-        return `No available Reports for ${class_name} for week ${week}`
+      else if (type === 'week' && week !== undefined) {
+         const report_files = report.getReportFromWeek(klassenName, week);
+         if (report_files) {
+            interaction.user.send({ content: `Reports for ${klassenName} from week ${week}`, files: report_files });
+            return `Sending a DM with Reports for ${klassenName} for week ${week}.`;
+         }
+         else {
+            return `No available Reports for ${klassenName} for week ${week}`;
+         }
       }
-    }
-  },
-}
+   },
+};
