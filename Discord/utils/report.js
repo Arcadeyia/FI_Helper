4b955159abf911d47aae43cfb2ddae9d77719e1a
nyxb <contact@nyxb.email>
2023-11-23 22:43:43 +0100

    ♻️ refactor(Discord/utils/report.js): Vereinheitlicht die Verwendung von Semikolons und verbessert die Benennung von Variablen für Konsistenz und Lesbarkeit.

--- Discord/utils/report.js
+++ Discord/utils/report.js
@@ -1,44 +1,43 @@
-const fs = require('node:fs')
+const fs = require('fs');
 
-function getAllReports(class_name) {
-  const reportPath = `Report/${class_name}/`
-  const reportPackets = []
-  let reportFiles = []
-  let fileCount = 0
+function getAllReports(klassenName) {
+   const reportPath = `Berichte/${klassenName}/`;
+   const reportPackets = [];
+   let reportFiles = [];
+   let fileCount = 0;
 
-  fs.readdirSync(reportPath).forEach((file) => {
-    reportFiles.push(`${reportPath}${file}`)
-    fileCount++
+   fs.readdirSync(reportPath).forEach((file) => {
+      reportFiles.push(`${reportPath}${file}`);
+      fileCount++;
 
-    if (fileCount === 10) {
-      fileCount = 0
-      reportPackets.push(reportFiles)
-      reportFiles = []
-    }
-  })
+      if (fileCount === 10) {
+         fileCount = 0;
+         reportPackets.push(reportFiles);
+         reportFiles = [];
+      }
+   });
 
-  // Fügen Sie die verbleibenden Dateien zum letzten Paket hinzu, falls vorhanden
-  if (reportFiles.length > 0)
-    reportPackets.push(reportFiles)
+   // Fügen Sie die verbleibenden Dateien zum letzten Paket hinzu, falls vorhanden
+   if (reportFiles.length > 0)
+      reportPackets.push(reportFiles);
 
-  return reportPackets
+   return reportPackets;
 }
 
-function getReportFromWeek(class_name, week) {
-  const currentYear = new Date().getFullYear()
-  const reportPaths = []
+function getReportFromWeek(klassenName, week) {
+   const currentYear = new Date().getFullYear();
+   const reportPaths = [];
 
-  for (let weekIndex = 1; weekIndex <= 20; weekIndex++) {
-    const reportPath = `Report/${class_name}/KW${week}_${weekIndex}_${currentYear}.pdf`
+   for (let weekIndex = 1; weekIndex <= 20; weekIndex++) {
+      const reportPath = `Berichte/${klassenName}/KW${week}_${weekIndex}_${currentYear}.pdf`;
 
-    if (fs.existsSync(reportPath))
-      reportPaths.push(reportPath)
+      if (fs.existsSync(reportPath))
+         reportPaths.push(reportPath);
+      else
+         break;
+   }
 
-    else
-      break
-  }
-
-  return reportPaths.length === 0 ? false : reportPaths
+   return reportPaths.length === 0 ? false : reportPaths;
 }
 
-module.exports = { getAllReports, getReportFromWeek }
+module.exports = { getAllReports, getReportFromWeek };
