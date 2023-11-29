5bead58d6d72a37c7d6d33c3ece0a335792d3593
Arcadeyi <aurelia@arcadeyia.com>
2023-11-29 10:18:24 +0100

    ♻️refactor: Erstellung Initialisierungs Datei

--- Javascript/functions/initialisierung.js
+++ Javascript/functions/initialisierung.js
@@ -1,6 +1,34 @@
-function initialisierung(client) {
-  // für ordner in events require und argument client
+const fs = require('node:fs')
+const process = require('node:process')
+const ordner = require('../functions/erstelleordner')
+const cronjob = require('../cronjob/stundenplan.js')
 
+function initialisierung(client, klassenliste) {
+  ladeEvents(client)
+  ladeOrdner(client, klassenliste)
+  ladeCronjobs(client, klassenliste)
+}
+
+function ladeEvents(client) {
+  const pfad = `${process.cwd()}/Javascript/events/`
+  fs.readdirSync(pfad).forEach((file) => {
+    console.log(`${client.user.username} Lade Event "file"...`)
+    const event = require(`${pfad}${file}`)
+    event(client)
+    console.log(`${client.user.username} Event "file" geladen!`)
+  })
+}
+
+function ladeOrdner(client, klassenliste) {
+  console.log(`${client.user.username} Überprüfe ob Ordner vorhanden sind...`)
+  for (const klasse of klassenliste)
+    ordner(klasse)
+
+  console.log(`${client.user.username} Ordner Vorhanden!`)
+}
+
+function ladeCronjobs(client, klassenliste) {
+  cronjob(client, klassenliste)
 }
 
 module.exports = initialisierung
