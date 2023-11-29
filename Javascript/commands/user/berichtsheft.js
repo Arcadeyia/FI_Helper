12c2ae2b64a7f0cb994651b2fbd8b834e0fc0d78
Arcadeyi <aurelia@arcadeyia.com>
2023-11-29 13:54:54 +0100

    ♻️refactor:  Überarbeitung der Commands

---
+++ Javascript/commands/user/berichtsheft.js
@@ -0,0 +1,41 @@
+const { SlashCommandBuilder } = require('discord.js')
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('berichtsheft')
+    .setDescription('Forder Berichtshefte an.')
+    .addSubcommand(subcommand =>
+      subcommand
+        .setName('alle')
+        .setDescription('Fordere alle Berichtshefte der Klasese an.'),
+    )
+    .addSubcommand(subcommand =>
+      subcommand
+        .setName('woche')
+        .setDescription('Fordere Berichtshefte für eine bestimmte Woche an.')
+        .addIntegerOption(option =>
+          option.setName('woche')
+            .setDescription('Die gewünschte Woche des Berichtsheftes.')
+            .setRequired(true)
+            .setMaxValue(52)
+            .setMinValue(1),
+        )
+        .addIntegerOption(option =>
+          option.setName('jahr')
+            .setDescription('Das gewünschte Jahr des Berichtheftes.')
+            .setRequired(true))),
+  async execute(interaction, client, klasse) {
+    const subcmd = interaction.options.getSubcommand()
+    const woche = interaction.options.getInteger('woche')
+    const jahr = interaction.options.getInteger('jahr')
+
+    if (subcmd === 'alle') {
+      await interaction.reply({ content: `Sende Verfügbare PDFs für ${klasse.name} in den DMs!`, ephemeral: true })
+      klasse.sendeAlleDokumente('berichtsheft', interaction.user)
+    }
+    else {
+      await interaction.reply({ content: `Sende Verfügbare PDFs für ${klasse.name} der Woche ${woche} in den DMs!`, ephemeral: true })
+      klasse.sendeWochenDokumente('berichtsheft', interaction.user, woche, jahr)
+    }
+  },
+}
